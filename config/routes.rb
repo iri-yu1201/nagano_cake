8d570948308ec6e58b174f2950c1995d321f2478
iri-yu1201 <iri12yuta01@gmail.com>
2021-06-04 14:03:19 +0000

    [Add]adminのトップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root to: 'homes#top'
+    #resources :sessions, only: [:new, :create, :destroy]
+  end
+  
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'about' => 'homes#about'
-
 
 end
