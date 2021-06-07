e2afb21b05d46bdd1b31ad60a3bf55d957f4dd3a
iri-yu1201 <iri12yuta01@gmail.com>
2021-06-07 12:35:38 +0000

    [Add]管理者商品ページ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,12 +6,22 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @genres = Genre.all
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
     @item.save
     redirect_to admin_item_path(:item_id)
+  end
+
+  def show
+    @item =Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre, :price, :is_active)
   end
 
 end
