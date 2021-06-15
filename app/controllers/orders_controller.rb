8bff6a0f97e9a3c2f374abfca459034789da67de
iri-yu1201 <iri12yuta01@gmail.com>
2021-06-15 13:13:34 +0000

    [Fix]

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def comfire
+  end
+
+  def compleate
+  end
+
+end
