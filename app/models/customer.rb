9979c6db417f3f189a9b3c85b45465280d439a1d
iri-yu1201 <iri12yuta01@gmail.com>
2021-06-15 13:15:00 +0000

    [Update]顧客トップ、商品ページ

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,11 +4,6 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  before_create :create_name
-  def create_name
-    self.name = "#{first_name} #{last_name}"
-  end
-
   has_many :orders, dependent: :destroy
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
